f21084701bdb7ab40a8a2ed039e87c22643b405f
EC2 Default User <ec2-user@ip-10-1-0-37.ec2.internal>
2022-09-07 11:16:12 +0000

    me

--- src/App.js
+++ src/App.js
@@ -42,59 +42,85 @@ function App() {
   return (
     <div className="App">
       
-        <h1>SCREEN</h1>
+        <h1>Backpackwindow</h1>
         < Backpackwindow/>
         < br/>
+        <h1>BarLine</h1>
         < BarLine/>
         < br/>
+        <h1>Buildingthecdw</h1>
         < Buildingthecdw />
         < br/>
+        <h1>Chat</h1>
         < Chat/>
         < br/>
+        <h1>ChatbotTasks</h1>
         < ChatbotTasks/>
         < br/>
+        <h1>Creatingcdwprofile</h1>
         < Creatingcdwprofile/>
         < br/>
+        <h1>Goodtogo</h1>
         < Goodtogo/>
         < br/>
+        <h1>Loadingscreen</h1>
         < Loadingscreen/>
         < br/>
+        <h1>Logoforconferenceevent</h1>
         < Logoforconferenceevent /> 
         < br />
+        <h1>Firstoption</h1>
         < Firstoption />
         < br/>
+        <h1>Fewoptions</h1>
         < Fewoptions />
         < br/>
+        <h1>Fewoptions</h1>
         < Findfewoptions />
         < br />
+        <h1>Group1120</h1>
         < Group1120 />
         < br />
+        <h1>ORB</h1>
         < ORB />
-        < br />
+         < br />
+         <h1>Order50</h1>
         < Order50 />
         < br />
+        <h1>Rectangle1557</h1>
         < Rectangle1557 />
         < br />
+        <h1>Rectangle1558</h1>
         < Rectangle1558 />
         < br />
+        <h1>ReviewTechSystemOrder</h1>
         < ReviewTechSystemOrder />
         < br />
+        <h1>Simplewindow</h1>
         < Simplewindow />
         < br />
+        <h1>Singlebackpack</h1>
         < Singlebackpack />
         < br />
+        <h1>SupplierCallout</h1>
         < SupplierCallout />
         < br  />
+        <h1>Suppliers</h1>
         < Suppliers />
         < br />
+        <h1>TabletNav</h1>
         < TabletNav />
         < br />
+        <h1>Tasks</h1>
         < Tasks />
         < br />
+        <h1>Vendorcreationform</h1>
         < Vendorcreationform />
         < br />
+        <h1>VendorListBox</h1>
         < VendorListBox />
         < br />
+        <h1>VendorReferal</h1>
         < VendorReferal />
         
         
